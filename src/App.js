1da808946ef9e77a9d1fffe33001968754122d22
microlook! <55187887+sudhirdontha72@users.noreply.github.com>
2020-12-01 23:34:19 +0530

    App.js updated
    Simple pure React syntax,rendering the App component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
-hello = () => {
-    return "Hello World!";
-  }
+const App = () =>{
+    return React.createElement(
+        "div",
+        {},
+    React.createElement("h1",{},"Adopt me!")
+    );
+};
+
+    ReactDOM.render(
+        React.createElement(App),
+        document.getElementById("root")
+    );
